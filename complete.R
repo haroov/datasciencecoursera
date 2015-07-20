0a0cd4f7ebeb50622cec75f7211c88934a963af6
Liav Geffen <liav@geffen.org.il>
2015-07-20 15:53:11 +0300

    Update complete.R

--- complete.R
+++ complete.R
@@ -1,1 +1,17 @@
+complete <- function(directory = "specdata", id = 1:332) {
+        
+        path_files <- paste(directory,"//*.csv", sep = "")
+        
+        files <- (Sys.glob(path_files))
+        nobs <- c();
+        
+        for (index in id) {
+                db <- read.csv(files[index], sep = ",");
+                complete_cases <- db[complete.cases(db),];
+                nobs <- c(nobs, nrow(complete_cases));
+        }
+        
+        return(data.frame(cbind(id, nobs)));
+}
+
 
