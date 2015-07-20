9cf3639db9d9ae88ad7a4a8a8265432d2ce4df07
Liav Geffen <liav@geffen.org.il>
2015-07-20 16:03:58 +0300

    Update corr.R

--- corr.R
+++ corr.R
@@ -1,13 +1,21 @@
-corr <- function(directory, threshold = 0) {
-
-        ## 'directory' is a character vector of length 1 indicating
-        ## the location of the CSV files
+corr <- function(directory = "specdata", threshold = 0) {
         
-        ## 'threshold' is a numeric vector of length 1 indicating the
-        ## number of completely observed observations (on all
-        ## variables) required to compute the correlation between
-        ## nitrate and sulfate; the default is 0
+        path_files <- paste(directory,"//*.csv", sep = "")
         
-        ## Return a numeric vector of correlations
-        ## NOTE: Do not round the result!
+        files <- (Sys.glob(path_files))
+        
+        correlations <- c()
+        
+        for (file in files) {
+                
+                db <- read.csv(file, sep = ",");
+                complete_cases <- db[complete.cases(db),];
+                
+                if (nrow(complete_cases) > threshold) {
+                        correlations <-
+                                c(correlations, cor(complete_cases$sulfate, complete_cases$nitrate))
+                }
+        }
+        
+        return(correlations)
 }
